2621f0cb363ec85c227bf20cf4e80cf826395be3
iskaz-12 <60395751+iskaz-12@users.noreply.github.com>
2023-11-08 00:57:05 +0300

    Delete build directory

--- build/CMakeFiles/charm_3d_v2.dir/src/method/Reaction.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/charm_3d_v2.dir/src/method/Reaction.cpp.o: \
- /cygdrive/c/MAGDISS3D/THE\ NEWEST\ VERSION/src/method/Reaction.cpp \
- /cygdrive/c/MAGDISS3D/THE\ NEWEST\ VERSION/src/method/Reaction.h
--- build/contrib/yaml-cpp/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/c/MAGDISS3D/THE NEWEST VERSION/contrib/yaml-cpp/util
-# Build directory: /cygdrive/c/MAGDISS3D/THE NEWEST VERSION/build/contrib/yaml-cpp/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
