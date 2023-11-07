6e71c93be0fefcdf6fabb3b9d6cfd35a2ef05aba
iskaz-12 <60395751+iskaz-12@users.noreply.github.com>
2023-11-08 00:59:33 +0300

    Delete src/contrib/yaml-cpp directory

--- src/contrib/yaml-cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/c/MAGDISS3D/THE NEWEST VERSION/contrib/yaml-cpp
-# Build directory: /cygdrive/c/MAGDISS3D/THE NEWEST VERSION/src/contrib/yaml-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
